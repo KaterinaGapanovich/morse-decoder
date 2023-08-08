beb3d1279e26486a3a261c23fca0e332d284dfac
Katerina Gapanovich <katerina.kulik@inbox.ru>
2023-08-09 00:51:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,66 +1,69 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
-function decode(expr) {
-    
-        let result = '';
-        for (let i = 0; i < expr.length; i = i + 10) {
-          let decodedLetter = expr.substring(i, i + 10);
-          if (decodedLetter == '**********') {
-            result += ' ';
-            continue;
-          }
-          let code = '';
-          for (let m = 0; m < decodedLetter.length; m +=2) {
-            let word = decodedLetter.substring(m, m + 2);
-            if (word == '10') {
-              code += '.';
-            } else if (word == '11') {
-              code += '-';
-            }
-          }
-          result += MORSE_TABLE[code];
-        }
-        return result;
-      
+function decode(words) {
+
+  let myResult = '';
+
+  for (let i = 0; i < words.length; i = i + 10) {
+
+    let decLetter = words.substring(i, i + 10);
+    if (decLetter == '**********') { myResult += ' ';
+      continue;
+    }
+
+    let newCode = '';
+
+    for (let m = 0; m < decLetter.length; m += 2) {
+
+      let itsWord = decLetter.substring(m, m + 2);
+      if (itsWord == '10') { newCode += '.';
+      } else if (itsWord == '11') { newCode += '-';
+      }
+    }
+
+    myResult += MORSE_TABLE[newCode];
+  }
+
+  return myResult;
 }
 
 module.exports = {
-    decode
+  decode
 }
